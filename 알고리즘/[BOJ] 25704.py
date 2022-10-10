b6499d7dcdfca557410df72ed2c1cd6c374feedf
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-10 22:02:42 +0900

    [BOJ] 25704 출석 이벤트

---
+++ 알고리즘/[BOJ] 25704.py
@@ -0,0 +1,18 @@
+n = int(input())
+m = int(input())
+x = []
+if n >= 5:
+    x.append(m-500)
+if n >= 10:
+    x.append(m-((m//100)*10))
+if n >= 15:
+    x.append(m-2000)
+if n >= 20:
+    x.append(m-((m//100)*25))
+print(x)
+if x == []:
+    print(m)
+elif min(x) < 0 :
+    print(0)
+else:
+    print(min(x))
