89c809c979cb16681a77b132f223241c109079c5
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-10 13:42:54 +0900

    [BOJ] 11945 뜨거운 붕어빵

---
+++ 알고리즘/[BOJ] 11945.py
@@ -0,0 +1,14 @@
+import sys 
+n,m = map(int,sys.stdin.readline().split())
+result = []
+for i in range(n):
+    x = []
+    a = sys.stdin.readline().strip()
+    for l in a:
+        x.insert(0,l)
+    result.append("".join(x))
+for k in result:
+    print(k)
+    
+
+
