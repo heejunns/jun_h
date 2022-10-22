035bfb31fe9f0d99bf4089c6115ce7497236f36a
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-21 23:26:56 -0700

    자료형_실습_2

---
+++ 점프투 자바 실습/자료형/Java_exercise_2.java
@@ -0,0 +1,11 @@
+public class Java_exercise_2 {
+    public static void main(String[] args) {
+        int a = 13;
+        if (a%2 == 0){
+            System.out.println("13은 짝수 입니다.");
+        } else{
+            System.out.println("13은 홀수 입니다.");
+        }
+
+    }
+}
