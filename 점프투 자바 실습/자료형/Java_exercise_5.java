24135221ec342ce1d37b863745fb015b3034e6f7
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-21 23:27:58 -0700

    자료형_실습_5

---
+++ 점프투 자바 실습/자료형/Java_exercise_5.java
@@ -0,0 +1,12 @@
+public class Java_exercise_5 {
+    public static void main(String[] args) {
+        String a = "a:b:c:d";
+        String b = a.replaceAll(":","#");
+        System.out.println(b);
+
+
+
+
+
+    }
+}
