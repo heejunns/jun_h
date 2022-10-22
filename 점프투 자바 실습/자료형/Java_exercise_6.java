27a67b0dcd254c72c62f68bbf4c34883fc1fcee2
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-21 23:28:24 -0700

    자료형_실습_6

---
+++ 점프투 자바 실습/자료형/Java_exercise_6.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Java_exercise_6 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,3,5,4,2));
+        System.out.println(arrayList); // 기존 그대로 출력
+        arrayList.sort(Comparator.reverseOrder());
+        System.out.println(arrayList);
+
+
+
+
+
+    }
+}
