0a221c9fe3ecf113688f2a938d6c5881eb8c778d
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-21 23:27:38 -0700

    자료형_실습_4

---
+++ 점프투 자바 실습/자료형/Java_exercise_4.java
@@ -0,0 +1,8 @@
+public class Java_exercise_4 {
+    public static void main(String[] args) {
+        String secretnum = "881120-1068234";
+        System.out.printf("홍길동씨의 주민번호의 성별을 나타내는 숫자는:%s 입니다.",secretnum.charAt(7));
+
+
+    }
+}
