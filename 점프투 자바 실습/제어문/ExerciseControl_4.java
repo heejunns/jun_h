503c640f3675154ed8bdc25f6c21d2d9e772e299
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-22 00:00:08 -0700

    제어문_실습_4

---
+++ 점프투 자바 실습/제어문/ExerciseControl_4.java
@@ -0,0 +1,10 @@
+package Exercise_control;
+
+public class ExerciseControl_4 {
+    public static void main(String[] args) {
+       for (int i = 1; i < 101; i++){
+           System.out.println(i);
+       }
+    }
+
+}
