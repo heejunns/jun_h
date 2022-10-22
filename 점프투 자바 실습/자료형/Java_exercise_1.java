0703a2d89468a61ac1bb96e67e9d5019b1fbae78
heejunns <107148343+heejunns@users.noreply.github.com>
2022-10-21 23:26:16 -0700

    자료형_실습_1

---
+++ 점프투 자바 실습/자료형/Java_exercise_1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Java_exercise_1 {
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 75;
+        int c = 55;
+        int result = (a+b+c)/3;
+        System.out.printf("홍길동씨의 평균 점수는 %d점 입니다.",result);
+
+    }
+}
